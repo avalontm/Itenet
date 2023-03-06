0d6b4ef9f8b84afa6b26a8270a5789b73ee374ec
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2023-03-06 14:53:44 -0800

    updated

---
+++ Itenet/Models/Noticia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itenet.Models
+{
+    public class Noticia
+    {
+        public int id { set; get; }
+        public string titulo { set; get; }
+        public string mensaje { set; get; }    
+    }
+}
--- Itenet/Views/MainPage.xaml.cs
+++ Itenet/Views/MainPage.xaml.cs
@@ -1,16 +1,29 @@
 ﻿using Android.Database.Sqlite;
 using Firebase.Database;
 using Firebase.Database.Query;
+using Itenet.Models;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace Itenet.Views;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    ObservableCollection<Noticia> _noticias;
+    public ObservableCollection<Noticia> Noticias
+    {
+        get { return _noticias; }
+        set
+        {
+            _noticias = value;
+            OnPropertyChanged("Noticias");
+        }
+    }
 
 	public MainPage()
 	{
 		InitializeComponent();
+        BindingContext = this;
     }
 
     protected override void OnAppearing()
@@ -21,25 +34,16 @@ public partial class MainPage : ContentPage
 
     async void onGetTrabajos()
     {
-        var items = await FireBaseManager.Get<List<object>>("noticias");
-
-        foreach (var item in items)
-        {
-            Console.WriteLine($"{item}");
-        }
-
+        Noticias = await FireBaseManager.Get<ObservableCollection<Noticia>>("noticias");
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    async void onNoticiaTapped(object sender, TappedEventArgs e)
+    {
+        Border borde = sender as Border;
+        await borde.ScaleTo(0.98, 50);
+        await borde.ScaleTo(1, 50);
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
